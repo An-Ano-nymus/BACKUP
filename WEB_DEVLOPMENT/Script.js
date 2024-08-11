3a35ba03a95d6aeae24172e6240b68dd10c691ab
Raghav Garg <114868679+An-Ano-nymus@users.noreply.github.com>
2024-08-11 14:24:48 +0530

    Add files via upload

---
+++ WEB_DEVLOPMENT/Script.js
@@ -0,0 +1,19 @@
+let element1=document.querySelector('#NEVER_GIVE_UP1')
+
+
+
+function click(){
+
+
+    element1.innerHTML= "FIGHT FOR US";
+    element1.style.backgroundColor='Yellow';
+    element1.style.color='Black';
+    element1.style.fontSize='20px';
+
+
+}
+
+element1.onclick =click;
+
+
+
